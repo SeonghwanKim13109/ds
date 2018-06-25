54a59cb8ab999fe43374bf403133bdb37da53915
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-25 22:18:12 +0900

    number : key , count : value가 되는 class

--- DS_1_ex2/src/com/java/ds/FindPair.java
+++ DS_1_ex2/src/com/java/ds/FindPair.java
@@ -3,7 +3,28 @@ package com.java.ds;
 import java.util.ArrayList;
 
 public class FindPair {
-	public static<T> void findPair(ArrayList<T> arrList) {
-		
+	private int number;
+	private int count;
+	private int comb_num;
+	
+	public int getNumber() {
+		return number;
 	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getComb_num() {
+		return comb_num;
+	}
+	public void setComb_num() {
+		comb_num = Combination.combination(count, 2);
+	}
+	
+	
 }
