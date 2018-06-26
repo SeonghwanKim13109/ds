2e43c8b39b52ed0dcbabaedf8970ce5cd483e0d0
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-26 09:30:57 +0900

    배열에서 selection sort

--- DS_2_ex1/src/com/java/ds/SelectionSort.java
+++ DS_2_ex1/src/com/java/ds/SelectionSort.java
@@ -1,5 +1,12 @@
 package com.java.ds;
 
 public class SelectionSort {
-
+	public static void s_sort(int[] arr , int last_index , int data) {
+		int j = last_index-1;
+		while(j>=0&& arr[j]>data) {
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j+1] = data;
+	}
 }
