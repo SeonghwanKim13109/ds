1db5acebc3dc2943f73298efef8f5bae03ec4018
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-26 09:11:34 +0900

    최종 완성

--- DS_1_ex2/src/com/java/ds/Main.java
+++ DS_1_ex2/src/com/java/ds/Main.java
@@ -1,10 +1,21 @@
 package com.java.ds;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Integer> arrList = new ArrayList();
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			int temp = scan.nextInt();
+			if(temp <=0)
+				break;
+			arrList.add(temp);
+		}
+		Histogram.findNum(arrList);
 	}
 
 }
