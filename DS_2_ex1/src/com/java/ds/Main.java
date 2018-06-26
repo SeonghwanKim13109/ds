a1a9eab131910e88248c33cf4ee989070f0658ac
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-26 09:31:05 +0900

    최종

--- DS_2_ex1/src/com/java/ds/Main.java
+++ DS_2_ex1/src/com/java/ds/Main.java
@@ -1,10 +1,25 @@
 package com.java.ds;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int size;
+		Scanner scan = new Scanner(System.in);
+		size = scan.nextInt();
+		int[] arr = new int[size];
+		
+		for(int i =0; i<arr.length; i++) {
+			int data = scan.nextInt();
+			
+			SelectionSort.s_sort(arr, i, data);
+			
+			for(int j = 0 ; j<=i ; j++)
+				System.out.print(arr[j]+" ");
+			System.out.println();
+		}
 	}
 
 }
