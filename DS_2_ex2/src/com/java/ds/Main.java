99259bdd591ad97c52e59a261e7a75be111e20fb
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-26 12:11:25 +0900

    최종

--- DS_2_ex2/src/com/java/ds/Main.java
+++ DS_2_ex2/src/com/java/ds/Main.java
@@ -3,6 +3,8 @@ package com.java.ds;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -11,12 +13,18 @@ public class Main {
 			Scanner scan = new Scanner(new File("in.txt"));
 			ArrayList<String> arr_s = new ArrayList();
 			ArrayList<Integer> arr_i = new ArrayList();
+		
 			while(scan.hasNext()) {
 				String name = scan.next();
 				int num = scan.nextInt();
 				arr_s.add(name);
 				arr_i.add(num);
 			}
+			System.out.println(arr_s.toString());
+			System.out.println(arr_i.toString());
+			
+			BubbleSort.bubSort(arr_s, arr_i);
+			
 			System.out.println(arr_s.toString());
 			System.out.println(arr_i.toString());
 			scan.close();
