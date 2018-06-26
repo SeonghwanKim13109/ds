e719e5c8faa0b7f0cdabab489974f291f7bd55e5
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-26 11:28:21 +0900

    scanner를 이용하여 file 읽어보는 예제

---
+++ DS_2_ex2/src/com/java/ds/Main.java
@@ -0,0 +1,30 @@
+package com.java.ds;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			Scanner scan = new Scanner(new File("in.txt"));
+			ArrayList<String> arr_s = new ArrayList();
+			ArrayList<Integer> arr_i = new ArrayList();
+			while(scan.hasNext()) {
+				String name = scan.next();
+				int num = scan.nextInt();
+				arr_s.add(name);
+				arr_i.add(num);
+			}
+			System.out.println(arr_s.toString());
+			System.out.println(arr_i.toString());
+			scan.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
