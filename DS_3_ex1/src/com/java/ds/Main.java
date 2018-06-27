9fda7765e47628fd845f71f4a92df437f5e69e85
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-27 21:33:10 +0900

    txt 읽어서 단어 찾는 프로그램

---
+++ DS_3_ex1/src/com/java/ds/Main.java
@@ -0,0 +1,11 @@
+package com.java.ds;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		boolean continue_flag = true;
+		while(continue_flag) {
+			
+		}
+	}
+}
---
+++ DS_3_ex1/src/com/java/ds/Word.java
@@ -0,0 +1,8 @@
+package com.java.ds;
+
+public class Word {
+	private final int MAX = 100000;
+	private String [] words = new String[MAX];
+	private int[] count = new int[MAX];
+	
+}
