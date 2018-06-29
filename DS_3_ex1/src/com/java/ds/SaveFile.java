5480e5e3aefbb072040b2b9836c5bb98b4d1915c
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-29 16:40:32 +0900

    원하는 이름으로 출력 저장

---
+++ DS_3_ex1/src/com/java/ds/SaveFile.java
@@ -0,0 +1,21 @@
+package com.java.ds;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SaveFile {
+	public static void save(String fileName, Word word) {
+		try {
+			PrintWriter outFile = new PrintWriter(new FileWriter(fileName));
+			for(int i = 0; i<word.getLast_insertion(); i++) {
+				outFile.println(word.findWordAt(i) + " " + word.countWordAt(i));
+			}
+			outFile.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("save failed");
+		}
+	}
+}
