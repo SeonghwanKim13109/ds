dfa4b2d843737c76ef3c43aff831e3f2438d5c43
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-29 16:41:38 +0900

    save file 까지의 예제

--- DS_3_ex1/src/com/java/ds/Main.java
+++ DS_3_ex1/src/com/java/ds/Main.java
@@ -1,11 +1,43 @@
 package com.java.ds;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		Word word = new Word();
 		boolean continue_flag = true;
-		while(continue_flag) {
-			
+		while (continue_flag) {
+			String command;
+
+			System.out.print("$  ");
+			command = scan.next();
+
+			if (command.equals("read")) {
+				String fileName = scan.next();
+				word.makeIndex(fileName);
+
+			} else if (command.equals("find")) {
+				String temp = scan.next();
+				int index = word.findWord(temp);
+				if (index > -1) {
+					System.out.println("The word '" + temp + "' appear " + word.countWordAt(index) + " times");
+				} else {
+					System.out.println("The word '" + temp + "'doesn't appear ");
+
+				}
+			} else if (command.equals("help")) {
+				
+				System.out.println("function : read / find /save / exit");
+				
+			} else if (command.equals("save")) {
+				String fileName = scan.next();
+				SaveFile.save(fileName, word);
+			} else if (command.equals("exit")) {
+				continue_flag = false;
+			}
 		}
+		scan.close();
+
 	}
 }
