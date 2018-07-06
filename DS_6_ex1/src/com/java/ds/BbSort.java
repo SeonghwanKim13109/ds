7e6f15b996f80f191818d5db0bc3eda95332e980
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-06 16:09:59 +0900

    오름차순을 내림차순으로 변경

---
+++ DS_6_ex1/src/com/java/ds/BbSort.java
@@ -0,0 +1,15 @@
+package com.java.ds;
+
+public class BbSort {
+	public static void bubbleSort(Term[] terms,int nTerm) {
+		for(int i = 0; i<nTerm;i++) {
+			for(int j = nTerm-1-i ; j>0; j--) {
+				if(terms[j].getExp() > terms[j-1].getExp()) {
+					Term temp = terms[j];
+					terms[j] = terms[j+1];
+					terms[j+1] = temp;
+				}
+			}
+		}
+	}
+}
