59e30542cb26c7d42a97c58cd9546b234350556b
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-06 18:18:22 +0900

    최종수정

--- DS_6_ex1/src/com/java/ds/Main.java
+++ DS_6_ex1/src/com/java/ds/Main.java
@@ -1,16 +1,19 @@
 package com.java.ds;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		Scanner scan = new Scanner(System.in);
 		boolean isNotExit = true;
 		Polynomial[] polys = new Polynomial[100];
 		int polyNum = 0;
-		
+
 		while (isNotExit) {
 			String cmd;
 			System.out.print("$ ");
@@ -63,7 +66,7 @@ public class Main {
 					break;
 				}
 				Polynomial temp = polys[idx];
-				System.out.println(temp.calc(value));
+				System.out.printf("%.2f\n", temp.calc(value));
 
 			} else if (cmd.equals("print")) {
 				String name = scan.next();
@@ -82,14 +85,48 @@ public class Main {
 				}
 				Polynomial temp = polys[idx];
 				temp.print();
-				
+
 			} else if (cmd.equals("exit")) {
 				isNotExit = false;
+			} else if (cmd.equals("help")) {
+				boolean continue_help = true;
+				while (continue_help) {
+					System.out.println("create/add/calc/print/exit");
+					String fileName = scan.next();
+					if (fileName.equals("exit"))
+						break;
+					else {
+						try {
+							fileName = fileName.concat(".txt");
+							Scanner scan_txt = new Scanner(new File(fileName));
+							while (scan_txt.hasNext()) {
+								String temp = scan_txt.next();
+								if (temp.equals("\\n"))
+									System.out.println();
+								else
+									System.out.print(temp + " ");
+							}
+							System.out.println();
+							scan_txt.close();
+
+						} catch (FileNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
 			} else {
 				System.out.println("No such command");
 			}
 		}
 		scan.close();
+		/*
+		 * try { Scanner scan = new Scanner(new File("add.txt")); while(scan.hasNext())
+		 * { String temp = scan.next(); if(temp.equals("\\n")) System.out.println();
+		 * else System.out.print(temp + " "); } } catch (FileNotFoundException e) { //
+		 * TODO Auto-generated catch block e.printStackTrace(); }
+		 */
+
 	}
 
 }
