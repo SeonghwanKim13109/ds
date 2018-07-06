960c8032e4d5acf0d4df69a678dc1ac0358b4806
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-06 18:14:50 +0900

    오류 수정

--- DS_6_ex1/src/com/java/ds/BbSort.java
+++ DS_6_ex1/src/com/java/ds/BbSort.java
@@ -6,8 +6,8 @@ public class BbSort {
 			for(int j = nTerm-1-i ; j>0; j--) {
 				if(terms[j].getExp() > terms[j-1].getExp()) {
 					Term temp = terms[j];
-					terms[j] = terms[j+1];
-					terms[j+1] = temp;
+					terms[j] = terms[j-1];
+					terms[j-1] = temp;
 				}
 			}
 		}
