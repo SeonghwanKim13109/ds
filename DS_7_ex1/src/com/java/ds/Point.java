0d3b631a1aa559e7c2a697a26d98238051583362
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-09 17:52:28 +0900

    선을 구성하기 위한 점 구성

---
+++ DS_7_ex1/src/com/java/ds/Point.java
@@ -0,0 +1,20 @@
+package com.java.ds;
+
+public class Point {
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public Point(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+}
