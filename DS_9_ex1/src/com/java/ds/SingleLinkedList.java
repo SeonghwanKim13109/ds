172571b119ec02016c5a7b963bae8adde36494f8
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-16 21:27:57 +0900

    add invert 추가

---
+++ DS_9_ex1/src/com/java/ds/SingleLinkedList.java
@@ -0,0 +1,59 @@
+package com.java.ds;
+
+public class SingleLinkedList<T> {
+	private Node head;
+	private int size = 0;
+
+	public Node getHead() {
+		return head;
+	}
+
+	public SingleLinkedList() {
+		super();
+		this.head = null;
+	}
+
+	public void add(T data) {
+		Node temp;
+		if (head == null) {
+			temp = new Node(data);
+			head = temp;
+		} else {
+			temp = head;
+			while (temp.getNext() != null) {
+				temp = temp.getNext();
+			}
+			Node new_node = new Node(data);
+			temp.setNext(new_node);
+		}
+		size++;
+	}
+
+	public void print() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.toString());
+			temp = temp.getNext();
+		}
+		System.out.println();
+	}
+
+	public void invert() {
+		Node middle = head;
+		if (middle == null)
+			return;
+		Node lead = middle.getNext();
+		Node trail = null;
+		if (lead == null)
+			return;
+		while (lead != null && lead.getNext() != null) {
+			middle.setNext(trail);
+			trail = middle;
+			middle = lead;
+			lead = lead.getNext();
+		}
+		middle.setNext(trail);
+		lead.setNext(middle);
+		head = lead;
+	}
+}
