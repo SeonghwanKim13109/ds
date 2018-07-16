0a30fcc3e078a4bf30c89866db1b050c1264394f
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-16 21:27:39 +0900

    linkedlist의 node

--- DS_9_ex1/src/com/java/ds/Node.java
+++ DS_9_ex1/src/com/java/ds/Node.java
@@ -17,5 +17,14 @@ public class Node<T> {
 		this.next = next;
 	}
 	
+	public Node(T data) {
+		this.data = data;
+		this.next = null;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "data : " + this.data;
+	}
 	
 }
