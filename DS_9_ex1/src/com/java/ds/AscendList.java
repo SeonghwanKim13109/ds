69d5604db4add70c4c1867982cb5259608429ba1
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-16 22:40:29 +0900

    오름차순 내림차순으로 정렬된 linked list

---
+++ DS_9_ex1/src/com/java/ds/AscendList.java
@@ -0,0 +1,29 @@
+package com.java.ds;
+
+public class AscendList<Integer> extends SingleLinkedList<Integer> {
+
+	public void add(int data) {
+
+		Node new_node = new Node(data);
+		if (head == null)
+			head = new_node;
+		else if ((int)head.getData() > data) {
+				Node temp = head;
+				head = new_node;
+				new_node.setNext(temp);
+		} else {
+			Node temp = head;
+			while (temp.getNext()!=null &&(int)temp.getNext().getData() < data )
+				temp = temp.getNext();
+			Node next = temp.getNext();
+			temp.setNext(new_node);
+			new_node.setNext(next);
+		}
+		size++;
+	}
+	
+	public void print() {
+		System.out.println("Ascend");
+		super.print();
+	}
+}
