8ce17d893cd6b2679eb213191bbfcd16f978ec89
SeonghwanKim13109 <reasonby13@naver.com>
2018-07-19 12:06:36 +0900

    linked list

---
+++ DS_10_ex1/src/com/java/ds/SingleLinkedList.java
@@ -0,0 +1,63 @@
+package com.java.ds;
+
+public class SingleLinkedList<T> {
+	protected Node<T> head;
+	protected int size;
+	
+	public SingleLinkedList() {
+		// TODO Auto-generated constructor stub
+		head = null;
+		size = 0;
+	}
+
+	public Node<T> getHead() {
+		return head;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setHead(Node<T> head) {
+		this.head = head;
+	}
+	
+	public void add(T data) {
+		Node<T> new_node = Node.getInstance(data);
+		if(head == null)
+			head = new_node;
+		else {
+			Node<T> temp = head;
+			while(temp.getNext()!=null)
+				temp = temp.getNext();
+			temp.setNext(new_node);
+		}
+		size++;
+	}
+	
+	public void print() {
+		Node<T> temp = head;
+		while(temp!=null && temp.getNext()!=null) {
+			System.out.print(temp.getData().toString()+"->");
+			temp = temp.getNext();
+		}
+		System.out.println(temp.getData().toString());
+	}
+	
+	public void remove(T data) {
+		Node<T> temp = head;
+
+		if (temp.getData().equals(data))
+			head = temp.getNext();
+		else {
+			while (temp.getNext() != null && !temp.getNext().getData().equals(data))
+				temp = temp.getNext();
+			if (temp.getNext().getData().equals(data)) {
+				Node<T> next = temp.getNext().getNext();
+				temp.setNext(next);
+			}
+		}
+		/*System.out.println(data.toString()+" is removed");
+		print();*/
+	}
+}
